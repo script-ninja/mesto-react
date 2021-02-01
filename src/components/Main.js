57cbe0eb5e3f9068d27043a3643a0701f3099cbe
script-ninja <61618647+script-ninja@users.noreply.github.com>
2021-02-01 19:11:06 +0500

    release: v1.1.0

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,13 +31,14 @@ export default function Main(props) {
           <h2 className="gallery__message gallery__message_visible">Нет добавленных фотографий</h2>
         }
         {
-          props.cards.map(card =>
-            <Card key={card._id}
-              card={card}
-              onCardClick={props.onCardClick}
-              onCardLike={props.onCardLike}
-              onCardDelete={props.onCardDelete}
-            />
+          props.cards.map(card => (
+	          <Card key={card._id}
+	            card={card}
+	            onCardClick={props.onCardClick}
+	            onCardLike={props.onCardLike}
+	            onCardDelete={props.onCardDelete}
+	          />
+            )
           )
         }
       </section>
